e59e20f58431e603b4a507111b2cece85d01f105
Ramiro Sánchez Ubuntu <dosek1996@gmail.com>
2021-03-15 23:31:51 -0600

    piano Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Piano';
+  activarSonido(sonido: number) {
+    let audio = new Audio();
+    audio.src = `../assets/sonidos/note${sonido}.wav`;
+    audio.load();
+    audio.play();
+  }
 }
